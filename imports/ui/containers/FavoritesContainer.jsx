e7a819c9772faf90e409f0615c9bcd3be81e4ce7
jist <jistjoalal@gmail.com>
2019-04-12 01:13:44 -0700

    refactor data containers/trackers

--- imports/ui/containers/FavoritesContainer.jsx
+++ imports/ui/containers/FavoritesContainer.jsx
@@ -1,7 +1,6 @@
-import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 
-import Resources from '../../api/resources';
+import { currentUser, resources, author } from './data';
 
 import ResourceList from '../components/ResourceList';
 
@@ -9,28 +8,13 @@ const withFavorites = withTracker(({ match }) => {
   // resources favorited by user specified in route
   const { userId } = match.params;
   const query = { favoritedBy: { $elemMatch: { $eq: userId } } };
-  const page = Session.get('page') || 1;
-  const resourcesHandle = Meteor.subscribe('resources', query, page); 
-  let resources = Resources.find().fetch();
 
-  // loading + ready
-  const loading = !resourcesHandle.ready();
-
-  // user data
-  Meteor.subscribe('userData', Meteor.userId());
-  const user = Meteor.users.findOne({ _id: Meteor.userId() });
-  const authorUser = Meteor.users.findOne({ _id: userId });
-  const author = authorUser ? authorUser.emails[0].address : 'Jist';
-
-  // page stats
-  Meteor.call('resources.count', query, (err, res) => {
-    if (!err) Session.set('total', res);
-  });
-  const limit = 10 * page;
-  const total = Session.get('total');
-  const title = "Favorites";
-
-  return { resources, loading, limit, total, title, user, author };
+  return {
+    title: 'Favorites', 
+    ...currentUser(),
+    ...resources(query),
+    ...author(userId)
+  };
 });
 
 export default withFavorites(ResourceList);
--- imports/ui/containers/ResourceListContainer.jsx
+++ imports/ui/containers/ResourceListContainer.jsx
@@ -1,35 +1,18 @@
-import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 
-import Resources from '../../api/resources';
+import { currentUser, resources } from './data';
 
 import ResourceList from '../components/ResourceList';
 
-const withResources = withTracker(() => {
+const withResources = withTracker(_ => {
   // resources (matching query + page)
   const query = Session.get('query') || {};
-  const page = Session.get('page') || 1;
-  const resourcesHandle = Meteor.subscribe('resources', query, page);
-  let resources = Resources.find().fetch();
 
-  // loading
-  const loading = !resourcesHandle.ready();
-
-  // user data
-  Meteor.subscribe('userData', Meteor.userId());
-  const user = Meteor.users.find().fetch()[0];
-
-  // page stats
-  if (!loading) {  // only serve total count when done loading
-    Meteor.call('resources.count', query, (err, res) => {
-      if (!err) Session.set('total', res);
-    });
-  }
-  const limit = 10 * page;
-  const total = Session.get('total') || 0;
-  const title = "Resources";
-
-  return { resources, loading, limit, total, title, user };
-})
+  return {
+    title: 'Resources',
+    ...currentUser(),
+    ...resources(query),
+  };
+});
 
 export default withResources(ResourceList);
