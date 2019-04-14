f7baf7316ea8132f9c96c6c7d635625e60f2b7e9
jist <jistjoalal@gmail.com>
2019-04-14 04:54:01 -0700

    remove filetype restrictions

--- imports/api/s3/allowed.filetypes.js
+++ imports/api/s3/allowed.filetypes.js
@@ -1,21 +1,8 @@
-// MIME types allowed for upload to S3
-const allowedFileTypes = [
-  // images
-  "image/png",
-  "image/jpeg",
-  "image/gif",
-  // microsoft office
-  "application/vnd.ms-powerpoint",
-  "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-  "application/msword",
-  "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-  "application/onenote",
-  // misc
-  "application/pdf",
-  "text/plain",
-];
 
+// allows all files
+// - need to figure out how to allow the plethora of
+//   MS mime types
 Slingshot.fileRestrictions("files", {
-  allowedFileTypes,
+  allowedFileTypes: /.*/,
   maxSize: 10 * 1024 * 1024 // 10 MB (use null for unlimited).
 });
